933126e7a8f3e4ee5153f9d1ff199e05b29ef0c1
Will Petra <domeless717@gmail.com>
2024-05-16 05:10:05 -0700

    Delete c/morse.c

--- c/morse.c
+++
@@ -1,2 +0,0 @@
-W(i){i?W(i/2),putchar(46-i%2):0;}c;main(){while((c=toupper(getchar())-32)>0){if(c==0)putchar(47);else if((c-=13)>0&&c<64)W("___!_____-___)o.q,0j42x976=y3b5c0ef><gkl/1du"[c]-96),putchar(32);}}
-
